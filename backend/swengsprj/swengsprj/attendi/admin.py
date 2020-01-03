bedbd7c29af85b4d6f518d3ff865e88a454d5ddf
kahrmarc17 <45610213+kahrmarc17@users.noreply.github.com>
2020-01-03 11:54:46 +0100

    created admin.py

--- backend/swengsprj/swengsprj/attendi/admin.py
+++ backend/swengsprj/swengsprj/attendi/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class CourseAdmin(admin.ModelAdmin): pass
+
+class CourseSessionAdmin(admin.ModelAdmin): pass
+
+class AttendanceItemAdmin(admin.ModelAdmin): pass
+
+class StatisticAdmin(admin.ModelAdmin): pass
+
+class UserAdmin(admin.ModelAdmin): pass
+
+
+admin.site.register(Course,CourseSessionAdmin)
+admin.site.register(CourseSession,CourseSessionAdmin)
+admin.site.register(AttendanceItem,AttendanceItemAdmin)
+admin.site.register(Statistic,StatisticAdmin)
+admin.site.register(User,UserAdmin)
