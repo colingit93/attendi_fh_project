8ea63ae59a305412f4cfa0b0fa03f71ca297696f
colingit93 <colin.jochum@gmx.at>
2020-01-22 14:23:25 +0100

    added comment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     }
 }
 
-# Password validation
+# Password validation Security
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
